ac185b8f8de27d2a3924a8256f142b3a540b50e5
suhanbrevenkar <57946370+suhanbrevenkar@users.noreply.github.com>
2022-07-01 15:15:36 +0530

    Add files via upload

---
+++ week 3/week3.py
@@ -0,0 +1,46 @@
+'''
+Assume df is a pandas dataframe object of the dataset given
+'''
+
+import numpy as np
+import pandas as pd
+import random
+
+
+'''Calculate the entropy of the enitre dataset'''
+# input:pandas_dataframe
+# output:int/float
+def get_entropy_of_dataset(df):
+    # TODO
+    return entropy
+
+
+'''Return avg_info of the attribute provided as parameter'''
+# input:pandas_dataframe,str   {i.e the column name ,ex: Temperature in the Play tennis dataset}
+# output:int/float
+def get_avg_info_of_attribute(df, attribute):
+    # TODO
+    return avg_info
+
+
+'''Return Information Gain of the attribute provided as parameter'''
+# input:pandas_dataframe,str
+# output:int/float
+def get_information_gain(df, attribute):
+    # TODO
+    return information_gain
+
+
+
+
+#input: pandas_dataframe
+#output: ({dict},'str')
+def get_selected_attribute(df):
+    '''
+    Return a tuple with the first element as a dictionary which has IG of all columns 
+    and the second element as a string with the name of the column selected
+
+    example : ({'A':0.123,'B':0.768,'C':1.23} , 'C')
+    '''
+    # TODO
+    pass
